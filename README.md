8f5c5b8d73fa783201a4f1d9e57df57e5b4c2dd8
alexey <alexeybulmeni@gmail.com>
2021-12-01 10:20:19 -0800

    add day 1 and day 1 b

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # 2021-advent-of-code
 
 (inspired by https://github.com/timvisee/advent-of-code-2020)
-# 2021-advent-of-code
---
+++ day1b/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let lines = include_str!("../input.txt")
+        .lines()
+        .map(|i| i.parse::<usize>().unwrap())
+        .collect::<Vec<usize>>()
+        .windows(3)
+        .map(|i| i.iter().sum())
+        .collect::<Vec<usize>>();
+
+    let mut total = 0;
+
+    for pair in lines.windows(2) {
+        if pair[1] > pair[0] {
+            total = total + 1;
+        }
+    }
+
+    println!("{}", total)
+}
