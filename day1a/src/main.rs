eb859a7fc89320ef38c6308752d2f5aea4d36903
alexey <alexeybulmeni@gmail.com>
2022-04-11 10:26:34 -0700

    rename day1 folder to be more consisten

---
+++ day1a/src/main.rs
@@ -0,0 +1,17 @@
+pub fn main() {
+    let lines = include_str!("../input.txt")
+        .lines()
+        .map(|i| i.parse::<usize>().unwrap())
+        .collect::<Vec<usize>>();
+
+    let mut total = 0;
+
+    // the hardest part is finding these functions lol
+    for pair in lines.windows(2) {
+        if pair[1] > pair[0] {
+            total += 1;
+        }
+    }
+
+    println!("{}", total)
+}
